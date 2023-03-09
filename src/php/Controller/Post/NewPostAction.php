a426e331760e65d277526919fec16bfb4d5fc2ff
Vanhalf <vh.win32@gmail.com>
2023-03-09 20:52:06 +0300

    add new_post

--- src/php/Controller/Post/NewPostAction.php
+++ src/php/Controller/Post/NewPostAction.php
@@ -7,10 +7,12 @@ use App\Controller\Action;
 
 class NewPostAction extends Action 
 {
+  
     protected function action(array $params): void
     {
       $this->render('new_post.twig', [
         'active' => '/new_post'
     ]);
     }
+
 }
